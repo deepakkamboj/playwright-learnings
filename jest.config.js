8ac9e6b568205ba0a8655c61bd350bb5715b5eb2
Deepak Kamboj <deepakkamboj@gmail.com>
2021-05-12 18:31:10 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     preset: "jest-playwright-preset",
-    testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
+    testMatch: ["**/?(*.)+(spec|test).+(ts|js)"],
     transform: {
       "^.+\\.(ts)$": "ts-jest",
     },
